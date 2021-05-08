6e09f5b4b7d93a8ad4ce8c53c31789dd0d140e51
kebekchieva <77875041+kebekchieva@users.noreply.github.com>
2021-05-08 13:00:35 +0600

    6

---
+++ 6.py
@@ -0,0 +1,8 @@
+class Words:
+    def __enterWords(self):
+        print("Enter Words Here")
+
+letter = Words()
+letter.__enterWords()
+
+# при помощи инкапсуляции можно ограничивать возможность вызова некоторой части кода
