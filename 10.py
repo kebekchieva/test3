1b7913c703517d26e16c0a246f81574cf961b422
kebekchieva <77875041+kebekchieva@users.noreply.github.com>
2021-05-08 14:07:43 +0600

    10

---
+++ 10.py
@@ -0,0 +1,7 @@
+state = ("California", "Washington", "Florida")
+evrState = iter(state)
+
+print(next(evrState))
+print(next(evrState))
+
+# итераторы это объект, в котором можно перечислисть все находящиеся объекты
